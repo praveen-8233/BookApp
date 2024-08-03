3148ffbb1032c44e1a08b6b3ee5f1150c4b43151
praveen-8233 <prasha3613@gmail.com>
2024-08-04 00:00:30 +0530

    remove public access modifier from repository

--- src/main/java/com/book/BookRepository.java
+++ src/main/java/com/book/BookRepository.java
@@ -5,32 +5,31 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Objects;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, Integer> {
 
-    public List<Book> findByBookPriceGreaterThan(Double price);
+    List<Book> findByBookPriceGreaterThan(Double price);
 
-    public List<Book> findByBookName(String name);
+    List<Book> findByBookName(String name);
 
     //SQL EXAMPLE
     @Query(value = "Select * from book", nativeQuery = true)
-    public List<Book> getAllBooks();
+    List<Book> getAllBooks();
 
     //HQL Example
     @Query("from Book")
-    public List<Book> getBooks();
+    List<Book> getBooks();
 
     @Query(value = "Select book_name from book", nativeQuery = true)
-    public List<String> getAllBooksName();
+    List<String> getAllBooksName();
 
     @Query(" SELECT b.bookName FROM Book b " )
-    public List<String> getAllBooksName1();
+    List<String> getAllBooksName1();
 
     @Query(value = " Select book_id from book ", nativeQuery = true)
-    public List<Integer> getBookId();
+    List<Integer> getBookId();
 
     @Query(" Select b.id from Book b")
-    public List<Integer> getBookIds();
+    List<Integer> getBookIds();
 }
