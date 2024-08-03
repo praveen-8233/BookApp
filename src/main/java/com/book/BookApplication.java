24d9f032133e2fa183b1cf3efa582227ab93b2ef
praveen-8233 <prasha3613@gmail.com>
2024-08-03 23:40:08 +0530

    Added SQL and HQL

--- src/main/java/com/book/BookApplication.java
+++ src/main/java/com/book/BookApplication.java
@@ -6,30 +6,31 @@ import org.springframework.context.ConfigurableApplicationContext;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 @SpringBootApplication
 public class BookApplication {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		ConfigurableApplicationContext context = SpringApplication.run(BookApplication.class, args);
-		BookRepository repo = context.getBean(BookRepository.class);
+        ConfigurableApplicationContext context = SpringApplication.run(BookApplication.class, args);
+        BookRepository repo = context.getBean(BookRepository.class);
 
-		// This is find By Method where we will provide our method with find by and we have to use property name
-		//with find by so it will work.
-		List<Book> byBookPriceGreaterThan = repo.findByBookPriceGreaterThan(2500.00);
-		for(Book b : byBookPriceGreaterThan ){
-			System.out.println(b);
-			System.out.println(" Find By Method End ");
-		}
+        // This is find By Method where we will provide our method with find by and we have to use property name
+        //with find by so it will work.
+        List<Book> byBookPriceGreaterThan = repo.findByBookPriceGreaterThan(2500.00);
+        for (Book b : byBookPriceGreaterThan) {
+            System.out.print(b);
+            System.out.println(" Find By Method End ");
+        }
 
-		List<Book> byName =  repo.findByBookName("Angular");
-		for(Book b : byName){
-			System.out.println(b);
-			System.out.println(" Find By Name Method End ");
-		}
+        List<Book> byName = repo.findByBookName("Angular");
+        for (Book b : byName) {
+            System.out.println(b);
+            System.out.println(" Find By Name Method End ");
+        }
 
-		//For Saving Data save method will save data if it is not exist if data is present then it will update data
+        //For Saving Data save method will save data if it is not exist if data is present then it will update data
 		/*Book b = new Book();
 		b.setBookId(106);
 		b.setBookName("Thread");
@@ -43,7 +44,7 @@ public class BookApplication {
 		}
 		*/
 
-		// Printing the retrieved books for both ids which i am passing
+        // Printing the retrieved books for both ids which i am passing
 		/*List<Integer> ids = Arrays.asList(101, 102);
 		List<Book> allById = repo.findAllById(ids);
 		for (Book book : allById) {
@@ -51,7 +52,38 @@ public class BookApplication {
 
 		}*/
 
+        List<Book> books = repo.getAllBooks();
+        for (Book b : books) {
+            System.out.print(b);
+            System.out.println(" Custom SQL Ended ");
+        }
+
+        List<Book> books1 = repo.getBooks();
+		for(Book b : books1){
+			System.out.print(b);
+			System.out.println(" HQL method Ended ");
+		}
+        System.out.println(" This is counting of records ");
+        Long num  = repo.count();
+        System.out.println(num);
+
+        System.out.println(" This is the names of Books from SQL");
+        List<String> names = repo.getAllBooksName();
+        System.out.println(names);
+
+        System.out.println(" This is the names of Books from HQL ");
+        List<String> names1  = repo.getAllBooksName1();
+        System.out.println(names1);
+
+        System.out.println(" This is book id from SQL ");
+        List<Integer> id = repo.getBookId();
+        System.out.println(id);
+
+        System.out.println(" This is books id from HQL ");
+        List<Integer> ids = repo.getBookIds();
+        System.out.println(ids);
 
 
-	}
+    }
 }
+
--- src/main/java/com/book/BookRepository.java
+++ src/main/java/com/book/BookRepository.java
@@ -1,15 +1,36 @@
 package com.book;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Objects;
 
 @Repository
-public interface BookRepository extends JpaRepository<Book,Integer> {
+public interface BookRepository extends JpaRepository<Book, Integer> {
 
     public List<Book> findByBookPriceGreaterThan(Double price);
 
     public List<Book> findByBookName(String name);
 
+    //SQL EXAMPLE
+    @Query(value = "Select * from book", nativeQuery = true)
+    public List<Book> getAllBooks();
+
+    //HQL Example
+    @Query("from Book")
+    public List<Book> getBooks();
+
+    @Query(value = "Select book_name from book", nativeQuery = true)
+    public List<String> getAllBooksName();
+
+    @Query(" SELECT b.bookName FROM Book b " )
+    public List<String> getAllBooksName1();
+
+    @Query(value = " Select book_id from book ", nativeQuery = true)
+    public List<Integer> getBookId();
+
+    @Query(" Select b.id from Book b")
+    public List<Integer> getBookIds();
 }
